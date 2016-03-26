72427f9f7511efff4e24a06b09aeb17bb4ce30f2
Makman2 <makman@alice.de>
2016-03-26 04:26:24 +0100

    WIP

--- bears/markdown/MarkdownBear.py
+++ bears/markdown/MarkdownBear.py
@@ -1,36 +1,37 @@
 import json
-import re
 
-from coalib.bearlib.abstractions.Lint import Lint
-from coalib.bears.LocalBear import LocalBear
+from coalib.bearlib.abstractions.Linter import Linter
 
 
-class MarkdownBear(Lint, LocalBear):
-    executable = 'remark'
-    diff_message = "The text does not comply to the set style."
-    arguments = '--no-color --quiet'
-    gives_corrected = True
-    use_stdin = True
+@Linter(executable='remark',
+        provides_correction=True,
+        use_stdin=True,
+        diff_message='The text does not comply to the set style.')
+class MarkdownBear:
+    """
+    Raises issues against style violations on markdown files.
+    """
 
-    def run(self, filename, file,
-            markdown_bullets: str="-",
-            markdown_closed_headings: bool=False,
-            markdown_setext_headings: bool=False,
-            markdown_emphasis: str="*",
-            markdown_strong: str="*",
-            markdown_encode_entities: bool=False,
-            markdown_codefence: str="`",
-            markdown_fences: bool=True,
-            markdown_list_indent: str="1",
-            markdown_loose_tables: bool=False,
-            markdown_spaced_tables: bool=True,
-            markdown_list_increment: bool=True,
-            markdown_horizontal_rule: str='*',
-            markdown_horizontal_rule_spaces: bool=False,
-            markdown_horizontal_rule_repeat: int=3):
+    @staticmethod
+    def create_arguments(filename,
+                         file,
+                         config_file,
+                         markdown_bullets: str="-",
+                         markdown_closed_headings: bool=False,
+                         markdown_setext_headings: bool=False,
+                         markdown_emphasis: str="*",
+                         markdown_strong: str="*",
+                         markdown_encode_entities: bool=False,
+                         markdown_codefence: str="`",
+                         markdown_fences: bool=True,
+                         markdown_list_indent: str="1",
+                         markdown_loose_tables: bool=False,
+                         markdown_spaced_tables: bool=True,
+                         markdown_list_increment: bool=True,
+                         markdown_horizontal_rule: str='*',
+                         markdown_horizontal_rule_spaces: bool=False,
+                         markdown_horizontal_rule_repeat: int=3):
         """
-        Raises issues against style violations on markdown files.
-
         :param markdown_bullets:                Character to use for bullets
                                                 in lists. Can be "-", "*"
                                                 or "+".
@@ -78,7 +79,7 @@ class MarkdownBear(Lint, LocalBear):
                                                 horizontal rule character will
                                                 be repeated.
         """
-        self.remark_configs = {
+        remark_configs = {
             "bullet": markdown_bullets,                         # - or *
             "closeAtx": markdown_closed_headings,               # Bool
             "setext": markdown_setext_headings,                 # Bool
@@ -96,8 +97,8 @@ class MarkdownBear(Lint, LocalBear):
             "ruleSpaces": markdown_horizontal_rule_spaces,      # Bool
             "ruleRepetition": markdown_horizontal_rule_repeat,  # int
         }
-        config_json = json.dumps(self.remark_configs)
-        # Remove { and } (remark adds it on its own) and escape it
-        settings = re.escape(config_json[1:-1])
-        self.arguments += " --setting " + settings
-        return self.lint(filename, file)
+
+        config_json = json.dumps(remark_configs)
+        # Remove { and } as remark adds them on its own
+        settings = config_json[1:-1]
+        return '--no-color', '--quiet', '--setting', settings
