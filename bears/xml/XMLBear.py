6aeec8eac680d55ab44d8a3763b56e1ef2656258
Makman2 <mak95@arcor.de>
2016-03-30 04:15:40 +0200

    WIP

--- bears/xml/XMLBear.py
+++ bears/xml/XMLBear.py
@@ -1,32 +1,53 @@
 import itertools
 
-from coalib.bearlib.abstractions.Lint import Lint
+from coalib.bearlib.abstractions.Linter import Linter
 from coalib.bears.LocalBear import LocalBear
 from coalib.misc.Shell import escape_path_argument
 from coalib.settings.Setting import path, url
 
 
 def path_or_url(xml_dtd):
-    '''
+    """
     Coverts the setting value to url or path.
 
     :param xml_dtd: Setting key.
     :return:        Returns a converted setting value.
-    '''
+    """
     try:
         return url(xml_dtd)
     except ValueError:
         return path(xml_dtd)
 
 
-# TODO
-class XMLBear(LocalBear, Lint):
+@Linter(executable='xmllint',
+        use_stderr=True)
+class XMLBear:
+    """
+    Checks the code with ``xmllint``.
+    """
     executable = 'xmllint'
     diff_message = "XML can be formatted better."
     output_regex = r'(.*\.xml):(?P<line>\d+): (?P<message>.*)\n.*\n.*'
     gives_corrected = True
     use_stderr = True
 
+    @staticmethod
+    def create_arguments(filename, file, config_file,
+                         xml_schema: path = "",
+                         xml_dtd: path_or_url = ""):
+        """
+        :param xml_schema: ``W3C XML Schema`` file used for validation.
+        :param xml_dtd:    ``Document type Definition (DTD)`` file or
+                           url used for validation.
+        """
+        args = (filename,)
+        if xml_schema:
+            args += ('-schema', xml_schema)
+        if xml_dtd:
+            args += ('-dtdvalid', xml_dtd)
+        return args
+
+    # TODO: Integrate multi-output grabbing into the linter.
     def process_output(self, output, filename, file):
         if self.stdout_output:
             # Return issues from stderr and stdout if stdout is not empty
@@ -35,20 +56,3 @@ class XMLBear(LocalBear, Lint):
                 self._process_corrected(self.stdout_output, filename, file))
         else:  # Return issues from stderr if stdout is empty
             return self._process_issues(self.stderr_output, filename)
-
-    def run(self, filename, file,
-            xml_schema: path="",
-            xml_dtd: path_or_url=""):
-        '''
-        Checks the code with ``xmllint``.
-
-        :param xml_schema: ``W3C XML Schema`` file used for validation.
-        :param xml_dtd:    ``Document type Definition (DTD)`` file or
-                           url used for validation.
-        '''
-        self.arguments = "{filename} "
-        if xml_schema:
-            self.arguments += " -schema " + escape_path_argument(xml_schema)
-        if xml_dtd:
-            self.arguments += " -dtdvalid " + xml_dtd
-        return self.lint(filename, file)
